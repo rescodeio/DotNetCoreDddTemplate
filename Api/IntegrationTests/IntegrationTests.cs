ff1e798ca958e023801f8018f8f75a01ee3d688a
rescodeio <luis.rebla@gmail.com>
2020-07-18 00:13:40 +0200

    Added tests

---
+++ Api/IntegrationTests/IntegrationTests.cs
@@ -0,0 +1,4 @@
+namespace IntegrationTests
+{
+    public class IntegrationTests{}
+}
---
+++ Api/UnitTests/ReverseNameUseCaseTest.cs
@@ -0,0 +1,45 @@
+using Domain;
+using Xunit;
+
+namespace UnitTests
+{
+    public class ReverseNameUseCaseTest
+    {
+        [Theory]
+        [InlineData("anna", WhitespacePolicy.DontIgnore, "anna", true)]
+        [InlineData("sarah haras", WhitespacePolicy.DontIgnore, "sarah haras", true)]
+        [InlineData("roger gregor", WhitespacePolicy.DontIgnore, "rogerg regor", false)]
+        [InlineData("walt law", WhitespacePolicy.DontIgnore, "wal tlaw", false)]
+        [InlineData("luis resco", WhitespacePolicy.DontIgnore, "ocser siul", false)]
+        public void CanReverseNames(string name, WhitespacePolicy whitespacePolicy, string reversed, bool isPalindrome)
+        {
+            var reverser = new ReverserNameUseCase();
+            var expected = new ReverseNameResponse(reversed, isPalindrome);
+            var result = reverser.Reverse(name, whitespacePolicy);
+            Assert.Equal(expected, result);
+        }
+        
+        [Theory]
+        [InlineData("roger gregor", WhitespacePolicy.Ignore, "rogerg regor", true)]
+        [InlineData("walt law", WhitespacePolicy.Ignore, "wal tlaw", true)]
+        [InlineData("luis resco", WhitespacePolicy.Ignore, "ocser siul", false)]
+        public void CanReverseNamesIgnoringSpaces(string name, WhitespacePolicy whitespacePolicy, string reversed, bool isPalindrome)
+        {
+            var reverser = new ReverserNameUseCase();
+            var expected = new ReverseNameResponse(reversed, isPalindrome);
+            var result = reverser.Reverse(name, whitespacePolicy);
+            Assert.Equal(expected, result);
+        }
+        
+        [Theory]
+        [InlineData("r a r", WhitespacePolicy.Ignore, "r a r", true)]
+        [InlineData("p o o", WhitespacePolicy.Ignore, "o o p", false)]
+        public void MultipleSpaces(string name, WhitespacePolicy whitespacePolicy, string reversed, bool isPalindrome)
+        {
+            var reverser = new ReverserNameUseCase();
+            var expected = new ReverseNameResponse(reversed, isPalindrome);
+            var result = reverser.Reverse(name, whitespacePolicy);
+            Assert.Equal(expected, result);
+        }
+    }
+}
